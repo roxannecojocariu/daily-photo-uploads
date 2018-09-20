e48a1d189f3ab4ec6db18e702743e898e8bea405
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-09-20 15:45:45 -0400

    add photo submission form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,8 +8,29 @@ class PhotosController < ApplicationController
     set_photo
   end
 
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.user = current_user
+
+    if @photo.save
+      flash[:notice] = "Photo was successfully submitted."
+      redirect_to photo_path(@photo)
+    else
+      flash[:notice] = @photo.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
   private
     def set_photo
       @photo = Photo.find(params[:id])
+    end
+
+    def photo_params
+      params.require(:photo).permit(:description, :image, :user)
     end
 end

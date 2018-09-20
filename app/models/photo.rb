7ca79a7abdf54e1f666352d1aa949a60190b5280
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-09-20 15:39:55 -0400

    add image size validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,11 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
+  validate :image_size_validation
   mount_uploader :image, ImageUploader
+
+private
+  def image_size_validation
+    errors[:image] << "should be less than 5MB" if image.size > 5.megabytes
+  end
 end

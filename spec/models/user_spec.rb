0feccc41cb61d898c0d333aeadc5fc2b41dbada2
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-09-23 21:47:22 -0400

    add user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  new_user = FactoryBot.create(:user)
+
+  it "instantiates a User object" do
+  	expect(new_user).to be_instance_of User
+  end
 end
